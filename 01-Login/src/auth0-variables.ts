ac85e6c14fc1300b6a8a61cec248078f50e23c49
Kyle Kemp <kyle@seiyria.com>
2017-05-15 08:49:19 -0500

    inline auth variables

--- 01-Login/src/auth0-variables.ts
+++
@@ -1,6 +0,0 @@
-export class Auth0Vars {
-  static AUTH0_DOMAIN = 'seiyria-auth0-samples.auth0.com';
-  static AUTH0_APPLICATION_PACKAGE_NAME = 'io.ionic.starter.auth0';
-  static AUTH0_CLIENT_ID = 'KeqqwaYkUfLiyTyMHiU06uaU1AjvDnX1';
-  static AUTH0_CALLBACK_URL = location.href;
-}
